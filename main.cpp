dc7356404c8e9524a884e26fa15f8f5ccfb5c1a7
Magalovsky <35817905+Magalovsky@users.noreply.github.com>
2018-02-13 21:49:36 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    string stuff(t, 'o');
+    cout << "W" << stuff << "w" << endl;
+    return 0;
+}
